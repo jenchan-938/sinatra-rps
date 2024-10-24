a5bb291327fdcc0e9d638cb319dfc4dc5a522d5d
jenchan-938 <jchan5@uchicago.edu>
2024-10-24 03:06:03 +0000

    10:05

--- app.rb
+++ app.rb
@@ -3,7 +3,52 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock-Paper-Scissors</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+@options = ["Rock", "Paper", "Scissors"]
+@robot = @options.sample
+@robot_play = "They played #{@robot}"
+
+get ("/playrock") do
+
+@user_play = "We played rock"
+if @robot == "Rock"
+  @result = "We tied"
+elsif @robot =="Paper"
+  @result = "We lost!"
+elsif @robot =="Scissors"
+    @result = "We won!"
+end
+
+end
+
+get ("/playscissors") do
+
+  @user_play = "We played scissors"
+
+  if @robot == "Scissors"
+    @result = "We tied"
+  elsif @robot =="Paper"
+    @result = "We won!"
+  elsif @robot =="Rock"
+      @result = "We lost!"
+  end
+  
+  end
+
+  get ("/playpaper") do
+
+    @user_play = "We played paper"
+    if @robot == "Paper"
+      @result = "We tied"
+    elsif @robot =="Rock"
+      @result = "We won!"
+    elsif @robot =="Scissors"
+        @result = "We lost!"
+    end
+    
+    end
